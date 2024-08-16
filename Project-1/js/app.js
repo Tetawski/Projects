8e4d94215d9ba1c5b13a7548ef11c00f217ce1c2
tetawskii bzz <t.bouzara@esi-sba.dz>
2024-08-16 19:03:32 +0100

    added the typing animation feature and added the dark mode

---
+++ Project-1/js/app.js
@@ -0,0 +1,27 @@
+const darkmode = document.getElementById('darkmode');
+const logo = document.getElementById('logo')
+
+
+let rootElement = document.documentElement;
+let cssVariableValue1 = getComputedStyle(rootElement).getPropertyValue('--text-primary-color').trim();
+let cssVariableValue2 = getComputedStyle(rootElement).getPropertyValue('--background-primary-color').trim();
+let isdark = false;
+darkmode.addEventListener('click' , () => {
+    if ( isdark !== true  ) {
+       
+        darkmode.style.filter = 'invert(100%)'
+        logo.style.filter = 'invert(100%)'
+        document.documentElement.style.setProperty('--text-primary-color' , '#f8f9fa' )
+        document.documentElement.style.setProperty('--background-primary-color' , '#000' )
+
+    }else {
+        darkmode.style.filter = 'invert(0%)'
+        logo.style.filter = 'invert(0%)'
+         
+        document.documentElement.style.setProperty('--text-primary-color' , '#000')
+        document.documentElement.style.setProperty('--background-primary-color' , '#f8f9fa')
+
+
+    }
+    isdark = !isdark;
+}) 
